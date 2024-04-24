3611e490d3d398c9ae56262889c89854fe753a66
BCM <dahifi@gmail.com>
2024-04-24 14:00:36 -0400

    test[oai]: sanity check on Threads and Messages

--- openai-assistant/test/test_threads.py
+++ openai-assistant/test/test_threads.py
@@ -1,13 +1,18 @@
 import unittest
 
-from openai.resources.beta.threads import Threads
 from utils.openai_utils import initialize_openai_client
-
-client = initialize_openai_client()
 
 
 class TestThreads(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.thread_id = "thread_PPecfP37eSXH14froEsJJCSr"
+        self.client = initialize_openai_client()
+
     async def test_threads(self):
-        thread_id = "thread_PPecfP37eSXH14froEsJJCSr"
-        response = await client.beta.threads.retrieve(thread_id)
+
+        response = await self.client.beta.threads.retrieve(self.thread_id)
+        print(response)
+
+    async def test_messages(self):
+        response = await self.client.beta.threads.messages.list(self.thread_id)
         print(response)
