be4a16ce6f3d3011faea8fc2ec5b85b63260c192
BCM <dahifi@gmail.com>
2024-04-24 13:22:49 -0400

    fixing fixtures

--- openai-assistant/test/test_annotations.py
+++ openai-assistant/test/test_annotations.py
@@ -3,51 +3,91 @@ import unittest
 
 from dotenv import load_dotenv
 
-# New Annotations
-from utils.annotations import construct_value_with_citations, extract_citations, create_annotations_list, \
-    validate_indexes, has_annotations
-from fixture import message_with_citation, message_with_citation_2, mock_message_with_multiple_annotations, \
-    message_no_citation
+from utils.annotations import OpenAIAdapter
+from fixture import message_with_citation, \
+    message_no_citation, message_with_invalid_index, message_with_multiple_annotations_no_quotes, \
+    message_with_multiple_annotations, message_with_no_quote
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 
-class NewTestAnnotations(unittest.TestCase):
-    def test_extract_citations(self):
-        result = extract_citations(mock_message_with_multiple_annotations)
-        self.assertEqual(len(result), 3)
-        self.assertEqual(result[0]['name'], 'Supplemental Agreement - Other')
-        result = extract_citations(message_with_citation)
-        self.assertEqual(result[0]['name'], 'source')
-        result = extract_citations(message_with_citation_2)
-        self.assertEqual(result[0]['name'], 'ce')
+annotations_fixtures = [message_with_citation,  # 1
+                        message_with_invalid_index,  # 0
+                        message_no_citation,  # 0
+                        message_with_multiple_annotations_no_quotes,  # 0
+                        message_with_multiple_annotations,  # 3
+                        message_with_no_quote  # 0
+                        ]
 
-    def test_construct_value_with_citations(self):
-        result = construct_value_with_citations(mock_message_with_multiple_annotations)
-        assert result
 
-        # This one is buggy because the OAI object doesn't align properly
-        result = construct_value_with_citations(message_with_citation_2)
-        pass
+class NewTestAnnotations(unittest.IsolatedAsyncioTestCase):
 
-    def test_create_annotations_list(self):
-        from chainlit.context import ChainlitContextException
-        citations = extract_citations(mock_message_with_multiple_annotations)
-        with self.assertRaises(ChainlitContextException):
-            result = create_annotations_list(citations)
+    async def test_all_fixtures(self):
+        results = []
+        expected_results = [1, 0, 0, 0, 3, 0]
+        for message in annotations_fixtures:
+            fixture = OpenAIAdapter(message)
+
+            await fixture.main()
+
+            self.assertTrue(fixture.get_content())
             pass
 
+            results.append(len(fixture.elements))
+
+        self.assertEqual(expected_results, results)
+
+    async def test_citation_details(self):
+        message = OpenAIAdapter(message_with_citation)
+        await message.set_citations()
+        message.validate_indexes()
+        message.set_elements()
+        self.assertTrue(message.elements)
+
+    async def test_extract_citations(self):
+
+        result = []
+
+        message = OpenAIAdapter(message_with_multiple_annotations_no_quotes)
+        await message.set_citations()
+        message.set_elements()
+        result = message.elements
+        self.assertEqual(len(result), 0)
+
+        message = OpenAIAdapter(message_with_multiple_annotations)
+        await message.set_citations()
+        message.set_elements()
+        result = message.elements
+        self.assertEqual(len(result), 3)
+
+        self.assertEqual(result[0][0], '[1] VA-AFGE-2023-Master-Agreement')
+        self.assertEqual(result[1][0], '[2] VA-AFGE-2023-Master-Agreement')
+        self.assertEqual(result[2][0], '[3] Supplemental Agreement - Other')
+
+    def test_construct_value_with_citations(self):
+
+        for message in [message_with_multiple_annotations_no_quotes, message_with_invalid_index, message_with_no_quote]:
+            result = OpenAIAdapter(message).get_content()
+            self.assertTrue(result)
+
+    async def test_create_annotations_list(self):
+        message = OpenAIAdapter(message_with_multiple_annotations_no_quotes)
+        await message.set_citations()
+        message.set_elements()
+        self.assertEqual(len(message.annotations), 3)
+
     def test_validate_indexes(self):
-        result = validate_indexes(message_with_citation)
+        result = OpenAIAdapter(message_with_citation).validate_indexes()
         self.assertTrue(result)
 
-        result = validate_indexes(message_with_citation_2)
+        result = OpenAIAdapter(message_with_invalid_index).validate_indexes()
         self.assertFalse(result)
 
     def test_has_annotations(self):
-        result = has_annotations(message_no_citation)
+        result = OpenAIAdapter(message_no_citation).has_annotations()
         self.assertFalse(result)
 
-        result = has_annotations(message_with_citation)
+        result = OpenAIAdapter(message_with_citation).has_annotations()
         self.assertTrue(result)
+
