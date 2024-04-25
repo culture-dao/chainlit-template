8c3cd333c86eb58891d26ca07f2e6ce14fbde4a7
BCM <dahifi@gmail.com>
2024-04-25 14:34:46 -0400

    fix: anonymize

--- openai-assistant/cl_events/on_chat_start.py
+++ openai-assistant/cl_events/on_chat_start.py
@@ -4,8 +4,8 @@ import chainlit as cl
 
 async def on_start_chat_logic(client):
     # Create a new thread with the OpenAI API
-    thread = await client.beta.threads.create()  # type: Thread
+    thread: Thread = await client.beta.threads.create()
     cl.user_session.set("thread", thread)
 
     # Send the initial welcome message to the user
-    await cl.Message(author="AFGE V.S.", content="Hi, I am the AFGE virtual steward! How can I help you today?").send()
+    await cl.Message(author="TEST AGENT", content="Hi, I am the test agent. What's up?").send()
