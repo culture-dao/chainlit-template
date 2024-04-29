7d451a0c70335e9b94a5b9f42d0dddb6c8b7b7c2
BCM <dahifi@gmail.com>
2024-04-29 13:11:33 -0400

    feat: chainlit runs

--- openai-assistant/app.py
+++ openai-assistant/app.py
@@ -19,15 +19,21 @@ client = initialize_openai_client()
 logger = logging.getLogger("chainlit")
 
 
-@cl.oauth_callback
-async def oauth_callback(provider_id: str, token: str, raw_user_data: Dict[str, str], default_app_user: cl.User) -> \
-        Optional[cl.User]:
-    return await oauth_callback_logic(provider_id, token, raw_user_data, default_app_user)
-
-
 @cl.on_chat_start
 async def on_chat_start_callback():
     return await on_start_chat_logic(client)
+
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
 
 
 @cl.author_rename
