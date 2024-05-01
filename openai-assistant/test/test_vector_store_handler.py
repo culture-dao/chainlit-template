f6fd4e2188227a058b3fda3ccd8013f54a1c696a
BCM <dahifi@gmail.com>
2024-05-01 10:22:30 -0400

    refactor: remove unused code

--- openai-assistant/test/test_vector_store_handler.py
+++ openai-assistant/test/test_vector_store_handler.py
@@ -5,7 +5,6 @@ from unittest.mock import MagicMock
 from openai.types.beta import VectorStore
 
 from utils import vector_stores_handler
-from utils.vector_stores_handler import file_comparison
 
 
 class TestVectorStoresHandler(unittest.IsolatedAsyncioTestCase):
@@ -29,57 +28,6 @@ class TestVectorStoresHandler(unittest.IsolatedAsyncioTestCase):
     async def test_vector_store_retrieve(self):
         store: VectorStore = await vector_stores_handler.vector_stores_retrieve(self.vector_id)
         self.assertTrue(isinstance(store, VectorStore))
-
-    # OLD SHIT
-
-    async def test_vectors_stores_files(self):
-        files = await vector_stores_handler.vector_stores_files()
-        self.assertEqual(len(files), 2, "Should return two files")
-
-    async def test_no_missing_or_extra_files(self):
-        oai_files = [{"name": "file1"}, {"name": "file2"}]
-        local_files = [{"name": "file1"}, {"name": "file2"}]
-
-        # Redirect logging output to a StringIO object for testing
-        with self.assertLogs() as logs:
-            file_comparison(oai_files, local_files)
-
-        self.assertEqual(logs.output, ["INFO:root:No extra files!", "INFO:root:No missing files!"])
-
-    async def test_missing_files(self):
-        oai_files = [{"name": "file1"}, {"name": "file2"}]
-        local_files = [{"name": "file1"}]
-
-        with self.assertLogs(level='INFO') as logs:
-            file_comparison(oai_files, local_files)
-        self.assertIn("INFO:root:Missing file locally: file2", logs.output)
-        self.assertIn("INFO:root:No extra files!", logs.output)
-
-    async def test_extra_files(self):
-        oai_files = [{"name": "file1"}]
-        local_files = [{"name": "file1"}, {"name": "file2"}]
-
-        with self.assertLogs(level='INFO') as logs:
-            file_comparison(oai_files, local_files)
-
-        self.assertIn("INFO:root:Extra file locally: file2", logs.output)
-        self.assertIn("INFO:root:No missing files!", logs.output)
-
-    def test_check_duplicate_files_usage(self):
-        duplicates = {
-            "file1.txt": ["id1", "id2"],
-            "file2.txt": ["id3"]
-        }
-        assistant_file_map = [
-            {"id": "assistant1", "files": ["id2"]},
-            {"id": "assistant2", "files": ["id3"]}
-        ]
-
-        expected_unused_files = ["id1"]
-
-        unused_files = check_duplicate_files_usage(duplicates, assistant_file_map)
-
-        self.assertEqual(unused_files, expected_unused_files, "The function should identify unused file IDs correctly.")
 
 
 if __name__ == '__main__':
