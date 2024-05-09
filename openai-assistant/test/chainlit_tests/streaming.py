12ee3858d29a1c07df1005cc6d23656a0a6c9ce1
BCM <dahifi@gmail.com>
2024-05-09 14:48:15 -0400

    test: new async streaming

---
+++ openai-assistant/test/chainlit_tests/streaming.py
@@ -0,0 +1,26 @@
+"""
+This is for testing the annotations or whatever other display functionality is needed
+"""
+
+import chainlit as cl
+
+from utils.event_handler import EventHandler
+from utils.openai_utils import initialize_openai_client
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    client = initialize_openai_client()
+    thread = await client.beta.threads.create()
+    async with client.beta.threads.runs.stream(
+            thread_id=thread.id,
+            assistant_id='asst_GPa9ziLBlAg4gmZXCq6L5nF9',
+            instructions="Please address the user as Jane Doe. The user has a premium account.",
+            event_handler=EventHandler(),
+    ) as stream:
+        await stream.until_done()
+
+if __name__ == "__main__":
+    from chainlit.cli import run_chainlit
+
+    run_chainlit(__file__)
