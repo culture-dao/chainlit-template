8d455857a1cf50064846a6c0982e9a73f0c74475
lauren1066 <laurenkeating364@gmail.com>
2024-05-13 14:02:33 -0400

    WIP: Give thread initial message

--- openai-assistant/test/chainlit_tests/streaming.py
+++ openai-assistant/test/chainlit_tests/streaming.py
@@ -5,6 +5,7 @@ import logging
 import chainlit as cl
 from utils.event_handler import EventHandler
 from utils.openai_utils import initialize_openai_client
+
 logging.basicConfig(level=logging.INFO)
 
 
@@ -12,13 +13,20 @@ logging.basicConfig(level=logging.INFO)
 async def on_chat_start():
     client = initialize_openai_client()
     thread = await client.beta.threads.create()
+    await client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content="What is the policy on annual leave?.",
+    )
+
     async with client.beta.threads.runs.stream(
             thread_id=thread.id,
             assistant_id='asst_GPa9ziLBlAg4gmZXCq6L5nF9',
             instructions="Please address the user as Jane Doe. The user has a premium account.",
-            event_handler=EventHandler(),
+            event_handler=EventHandler()
     ) as stream:
         await stream.until_done()
+
 
 if __name__ == "__main__":
     from chainlit.cli import run_chainlit
