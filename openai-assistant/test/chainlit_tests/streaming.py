f3e8fe4ac5db0d3e017946e25ea63face814de84
BCM <dahifi@gmail.com>
2024-05-14 13:49:24 -0400

    test: playground url

--- openai-assistant/test/chainlit_tests/streaming.py
+++ openai-assistant/test/chainlit_tests/streaming.py
@@ -6,17 +6,20 @@ import chainlit as cl
 from dotenv import load_dotenv
 
 from utils.event_handler import EventHandler
-from utils.openai_utils import initialize_openai_client
+from utils.openai_utils import initialize_openai_client, get_playground_url
 from cl_events.step import process_thread_message
 logging.basicConfig(level=logging.INFO)
 
 load_dotenv(dotenv_path='../', override=True)
 
+assistant_id='asst_2lanl1dvlTkCpOofxiPrHvzr'
 
 @cl.on_chat_start
 async def on_chat_start():
     client = initialize_openai_client('../../.env')
     thread = await client.beta.threads.create()
+
+    logging.info(get_playground_url(thread.id, assistant_id))
 
     # The question to ask the model
     question = 'Search the docs and tell me something about it.'
@@ -32,16 +35,17 @@ async def on_chat_start():
     cl_message = cl.Message(content=question, author="User")
     await cl_message.send()
 
-    e = EventHandler()
+    e: EventHandler = EventHandler()
 
     # Process the question and display the question in the UI
     async with client.beta.threads.runs.stream(
             thread_id=thread.id,
-            assistant_id='asst_2lanl1dvlTkCpOofxiPrHvzr',
-            instructions="Please address the user as Jane Doe. The user has a premium account.",
+            assistant_id=assistant_id,
+            instructions="Search the file and describe it in less than 20 words.",
             event_handler=e
     ) as stream:
         await stream.until_done()
+    pass
 
     # After the message has been processed, handle the annotations
     await process_thread_message(message_references=e.message_references, thread_message=e.openAIMessage, client=client)
