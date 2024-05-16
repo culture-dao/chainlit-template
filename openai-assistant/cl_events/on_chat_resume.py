88eb53e56fe679989451553a338924157c505a09
BCM <dahifi@gmail.com>
2024-05-16 14:14:20 -0400

    feat: better catching of on-chat-resume
    We still have a bug here where the message is being sent to Literal but not the UX.

--- openai-assistant/cl_events/on_chat_resume.py
+++ openai-assistant/cl_events/on_chat_resume.py
@@ -1,25 +1,30 @@
 import json
 import chainlit as cl
 from chainlit import ThreadDict, logger
+from openai import NotFoundError
 
 
-async def on_chat_resume_logic(thread: ThreadDict, client):
-    steps = thread.get('steps')
-
+async def on_chat_resume_logic(cl_thread: ThreadDict, client):
+    steps = cl_thread.get('steps')
+    response_content = "The old thread could not be fetched."
     # Find the right step that has the thread id we need.
-    # Once found, retrieve it and pass it to the  on_message function.
-    # Since we retrieved it and it's a thread, no modifications are needed to on_message.
+    # Once found, retrieve it and set it in the session.
+
     for step in steps:
-        if step['input'] != '':
+        if step['type'] == 'run':
             parsed_input = json.loads(step['input'])
             kwargs = parsed_input.get('kwargs', {})
             thread_id = kwargs.get('thread_id', None)
             logger.info(f"Resuming chat with thread ID: {thread_id}")
-            thread = await client.beta.threads.retrieve(thread_id)
+            try:
+                thread = await client.beta.threads.retrieve(thread_id)
+                if thread.id is None:
+                    pass
+                else:
+                    cl.user_session.set("thread", thread)
+                    response_content = "Welcome back! I'm ready to assist you. How can I help you today?"
+            except NotFoundError:
+                pass
             break
-    if thread.id is None:
-        response_content = "The old thread could not be fetched."
-    else:
-        response_content = "Welcome back! I'm ready to assist you. How can I help you today?"
-    cl.user_session.set("thread", thread)
-    await cl.Message(author="AFGE V.S.", content=response_content).send()
+    logger.info(response_content)
+    await cl.Message(content=response_content).send()
