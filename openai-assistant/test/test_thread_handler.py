e0adbb53a4a3e86e929bce9be94711656f9d7a4e
BCM <dahifi@gmail.com>
2024-05-21 16:34:10 -0400

    fix: annotations render citation source even if no quote
    (cherry picked from commit 081d1d0e340c5de55750c172e3cc37786d642002)

---
+++ openai-assistant/test/test_thread_handler.py
@@ -0,0 +1,29 @@
+import logging
+
+import chainlit as cl
+from app.utils.annotations import OpenAIAdapter
+from app.utils.openai_utils import get_thread_messages, initialize_openai_client
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("chainlit")
+
+client = initialize_openai_client()
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    messages = await get_thread_messages(client, 'thread_J05pHO7SIE0HWqQZJymAdo8Q')
+    logger.info(messages.data)
+    for thread_message in messages.data:
+        message = OpenAIAdapter(thread_message)
+        await message.set_citations()
+        cl_message = message.get_message()
+
+        logger.info(cl_message.elements)
+        await cl_message.send()
+
+
+if __name__ == "__main__":
+    from chainlit.cli import run_chainlit
+
+    run_chainlit(__file__)
--- openai-assistant/utils/annotations.py
+++ openai-assistant/utils/annotations.py
@@ -13,11 +13,10 @@ from openai.types import FileObject
 from openai.types.beta.threads import (
     Message as ThreadMessage
 )
-
 from openai.types.beta.threads.file_citation_annotation import FileCitation as TextAnnotationFileCitationFileCitation, \
     FileCitationAnnotation as TextAnnotationFileCitation
 
-from .openai_utils import initialize_openai_client
+from utils.openai_utils import initialize_openai_client
 
 
 class OpenAIAdapter:
@@ -26,7 +25,7 @@ class OpenAIAdapter:
     """
 
     def __init__(self, message: ThreadMessage) -> None:
-        self.client = initialize_openai_client("../../.env")
+        self.client = initialize_openai_client()
         self._id: str = message.id
         self.message: ThreadMessage = message
         # We're putting these in reverse order so we can work backward in get_content
