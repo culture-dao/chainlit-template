869540e93d37fc64133e03b6a550b1c8f4a05c30
BCM <dahifi@gmail.com>
2024-05-22 11:23:24 -0400

    refactor: on chat resume changes and test

--- openai-assistant/cl_events/on_chat_resume.py
+++ openai-assistant/cl_events/on_chat_resume.py
@@ -3,28 +3,36 @@ import json
 import chainlit as cl
 from chainlit import ThreadDict, logger
 
-from utils.assistant_handler import assistant_handler
 
-
-async def on_chat_resume_logic(thread: ThreadDict, client):
-    assistant = assistant_handler.find_by_name("Liminal Flow Agent")
+async def on_chat_resume_logic(thread: ThreadDict, client) -> None:
+    """
+    This function needs to make sure we can retrieve the original OAI Thread
+    """
 
     steps = thread.get('steps')
 
-    # Find the right step that has the thread id we need.
-    # Once found, retrieve it and pass it to the  on_message function.
-    # Since we retrieved it and it's a thread, no modifications are needed to on_message.
     for step in steps:
-        if step['input'] != '':
-            parsed_input = json.loads(step['input'])
-            kwargs = parsed_input.get('kwargs', {})
-            thread_id = kwargs.get('thread_id', None)
-            logger.info(f"Resuming chat with thread ID: {thread_id}")
-            thread = await client.beta.threads.retrieve(thread_id)
+        thread = await try_step(step, client)
+        if thread:
             break
+
     if thread.id is None:
         response_content = "The old thread could not be fetched."
     else:
         response_content = "Welcome back! I'm ready to assist you. How can I help you today?"
     cl.user_session.set("thread", thread)
+
     await cl.Message(content=response_content).send()
+
+
+async def try_step(step, client):
+    if step['type'] == 'run':
+        try:
+            parsed_input = json.loads(step['input'])
+            kwargs = parsed_input.get('kwargs', {})
+            thread_id = kwargs.get('thread_id', None)
+            logger.info(f"Resuming chat with thread ID: {thread_id}")
+            return await client.beta.threads.retrieve(thread_id)
+
+        except json.JSONDecodeError:
+            raise ValueError("Invalid JSON format")
---
+++ openai-assistant/test/test_chat_resume.py
@@ -0,0 +1,62 @@
+import unittest
+
+from openai.types.beta import Thread
+
+from utils.openai_utils import initialize_openai_client
+
+step_without_id = {
+    "createdAt": "2024-03-28T16:22:22.629Z",
+    "threadId": "85053762-ba5d-49e7-81bd-c72abb134c96",
+    "parentId": "9adcabb4-081f-48c0-90e4-e6501cf2557b",
+    "feedback": None,
+    "start": "2024-03-28T12:22:21.000Z",
+    "end": "2024-03-28T12:22:25.000Z",
+    "type": "tool",
+    "name": "retrieval",
+    "generation": None,
+    "input": "Retrieving information",
+    "output": "Retrieved information",
+    "showInput": None,
+    "disableFeedback": True,
+    "indent": None,
+    "language": None,
+    "isError": False,
+    "waitForAnswer": None,
+}
+
+step_with_id = {
+    "createdAt": "2024-05-16T15:41:50.529Z",
+    "id": "63c36aad-2322-49bd-a713-e0eec77cc331",
+    "threadId": "d5074bf8-729b-4fbd-a783-ee6d2ab310ef",
+    "parentId": None,
+    "feedback": None,
+    "start": "2024-05-16T15:41:51.098Z",
+    "end": "2024-05-16T15:42:15.089Z",
+    "type": "run",
+    "name": "AFGE V.S.",
+    "generation": None,
+    "input": '{\n    "args": [],\n    "kwargs": {\n        "thread_id": "thread_CrU3GtC9roS2Gt1VevPEWMtk",\n        '
+             '"human_query": "what is the policy on annual leave?",\n        "file_ids": []\n    }\n}',
+    "output": "",
+    "showInput": False,
+    "disableFeedback": True,
+    "indent": None,
+    "language": None,
+    "isError": False,
+    "waitForAnswer": None,
+}
+
+
+class TestChatResume(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.client = initialize_openai_client("../.env")
+
+    async def test_try_step(self):
+        from app.cl_events.on_chat_resume import try_step
+        result = await try_step(step_without_id, self.client)
+        self.assertEqual(None, result)
+
+    async def test_step_valid(self):
+        from app.cl_events.on_chat_resume import try_step
+        result = await try_step(step_with_id, self.client)
+        self.assertEqual(Thread, type(result))
