211b3af8a52ba96c64f84d6920b868de4ab3bdf4
BCM <dahifi@gmail.com>
2024-05-22 13:25:40 -0400

    refactor: pull webhook to env

--- openai-assistant/test/test_webhook.py
+++ openai-assistant/test/test_webhook.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from unittest import TestCase
 
 from cl_events.oauth_callback import post_to_discord
@@ -7,11 +8,7 @@ from fixture import raw_user_data
 
 class TestWebhook(TestCase):
     def test_webhook(self):
-        TEST_WEBHOOK=("https://discord.com/api/webhooks/1214278085390766090"
-                      "/hOsdCh799GvhcK5dadAXh_9GMaFNknx3zvAWy98V0dXvLYY7uDwZ_ja18AFhCwuqe70H")
-
-        # Replace 'your_webhook_url' with your actual Discord webhook URL
-        webhook_url = TEST_WEBHOOK
+        webhook_url = os.getenv('DISCORD_WEBHOOK')
         action = 'Ran unit tests'  # This would be dynamically replaced with the user's action
 
         asyncio.run(post_to_discord(webhook_url, raw_user_data, action))
