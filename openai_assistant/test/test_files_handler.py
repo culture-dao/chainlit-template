1c33939c0de2e916f44a41d5f5d1e91fd0f68209
BCM <dahifi@gmail.com>
2024-05-24 11:51:01 -0400

    feat: files handler
    We had to refactor the OAI mapping to use file IDs, since files have filenames, not names, and we need to get a handle on duplicate files also. So updated the utils and made sure the tests are working.

--- openai_assistant/test/test_files_handler.py
+++ openai_assistant/test/test_files_handler.py
@@ -3,9 +3,10 @@ import unittest
 from typing import List, Iterable
 
 from dotenv import load_dotenv
+from openai._types import FileTypes
 from openai.types import FileObject
 
-from utils import files_handler
+from utils.files_handler import files_handler
 
 load_dotenv('../.env', override=True)
 TEST_FILE_ID = os.getenv('TEST_FILE_ID')
@@ -14,11 +15,19 @@ TEST_FILE_ID = os.getenv('TEST_FILE_ID')
 class TestFilesHandler(unittest.IsolatedAsyncioTestCase):
 
     async def test_files_list(self):
-        files: List[FileObject] = await files_handler.files_list()
+        files: List[FileObject] = await files_handler._files_list()
         self.assertTrue(isinstance(files, Iterable), "ojb should be an iterable")
         self.assertTrue(all(isinstance(item, FileObject) for item in files),
                         "all items in files should be of type FileObject")
 
     async def test_file_retrieve(self):
-        result = await files_handler.files_retrieve(TEST_FILE_ID)
+        result = await files_handler._files_retrieve(TEST_FILE_ID)
         self.assertTrue(isinstance(result, FileObject), "ojb should be an FileObject")
+
+    async def test_file_create(self):
+
+        file_name = 'test/dummy_file.txt'
+        file_content = bytes(100)
+        file: FileTypes = (file_name, file_content)
+        result = await files_handler.create(file)
+        self.assertTrue(result.filename, file_name)
--- openai_assistant/utils/files_handler.py
+++ openai_assistant/utils/files_handler.py
@@ -3,6 +3,7 @@ import logging
 from typing import List
 
 from openai import NotFoundError
+from openai._types import FileTypes
 from openai.pagination import AsyncPage
 from openai.types import FileObject, FileDeleted
 
@@ -25,8 +26,8 @@ class FileHandler(OpenAIHandler):
     async def list(self):
         return await self._files_list()
 
-    async def create(self, config):
-        return await self._files_create(config)
+    async def create(self, file):
+        return await self._files_create(file)
 
     async def retrieve(self, assistant_id):
         return await self._files_retrieve(assistant_id)
@@ -44,14 +45,20 @@ class FileHandler(OpenAIHandler):
             logging.error(f"Failed to list files due to an error: {e}")
             raise Exception("Failed to list files") from e
 
-    async def _files_create(self, config):
-        pass
+    async def _files_create(self, file: FileTypes):
+        # Should take one file or many, use batch updates.
+        result: FileObject = await self.client.files.create(
+            file=file,
+            purpose="assistants"
+        )
+        await self.client.files.wait_for_processing(result.id)
+        return result
 
     async def _files_delete(self,file_id):
         result: FileDeleted = await self.client.files.delete(file_id)
         assert result.deleted
 
-    async def _files_retrieve(self,file_id: str) -> FileObject:
+    async def _files_retrieve(self, file_id: str) -> FileObject:
         try:
             return await self.client.files.retrieve(file_id)
         except NotFoundError as e:
@@ -59,12 +66,12 @@ class FileHandler(OpenAIHandler):
             raise e
 
 
-file_handler: FileHandler = FileHandler(CONFIG_PATH)
+files_handler: FileHandler = FileHandler(CONFIG_PATH)
 
 
 async def main() -> FileHandler:
-    await file_handler.init()
-    return file_handler
+    await files_handler.init()
+    return files_handler
 
 if __name__ == '__main__':
     asyncio.run(main())
