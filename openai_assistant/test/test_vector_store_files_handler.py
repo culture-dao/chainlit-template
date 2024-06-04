40cac422a7a5385e5813ecbda178a964e2e5815d
BCM <dahifi@gmail.com>
2024-06-04 13:16:52 -0400

    feat: file list

--- openai_assistant/test/test_vector_store_files_handler.py
+++ openai_assistant/test/test_vector_store_files_handler.py
@@ -5,7 +5,7 @@ import unittest
 from dotenv import load_dotenv
 
 load_dotenv('../.env', override=True)
-from utils.vector_store_files_handler import vector_store_files_handler # noqa: E402
+from utils.vector_store_files_handler import vector_store_files_handler  # noqa: E402
 TEST_VECTORSTORE_ID = os.getenv('TEST_VECTORSTORE_ID')
 TEST_FILE_ID = os.getenv('TEST_FILE_ID')
 
@@ -17,5 +17,5 @@ class TestVectorStoresFileHandler(unittest.IsolatedAsyncioTestCase):
         self.handler = vector_store_files_handler
 
     async def test_list(self):
-        files = await self.handler.list()
+        files = await self.handler.list(TEST_VECTORSTORE_ID)
         pass
